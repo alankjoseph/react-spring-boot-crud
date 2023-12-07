6a3294d6bc1c75181beff0270c2242c9047e4d58
5703 - Alan Joseph <5703alj@manorama.net>
2023-12-07 18:01:39 +0530

    done with delete user

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,13 +46,13 @@ function Home() {
                   <td className="px-6 py-4 ">{user.username}</td>
                   <td className="px-6 py-4 ">{user.email}</td>
                   <td className="px-6 py-4 ">
-                  <button
+                    <button
                       type="button"
                       className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                     >
                       Edit
                     </button>
-                
+
                     <button
                       type="button"
                       className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
